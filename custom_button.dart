71d68c82fb1620b2b3eaedceaa9021a1a8c7042f
lojaina16 <117110373+lojaina16@users.noreply.github.com>
2024-09-08 11:44:31 +0300

    Add files via upload

---
+++ custom_button.dart
@@ -0,0 +1,27 @@
+import 'package:doghome/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+   CustomButton({required this.text,required this.routeName});
+  final String text;
+  final String routeName;
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        padding: EdgeInsets.symmetric(vertical: 30),
+        backgroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        )
+      ),
+        onPressed:(){
+          Navigator.pushNamed(context, routeName);
+    },
+        child: Text(text)
+    );
+  }
+}
+
+
+
